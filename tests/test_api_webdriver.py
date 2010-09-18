100b73f0e76e9ec9623c6e5061d4bae1ee74d3f0
Hugo Lopes Tavares <hltbra@gmail.com>
2010-09-18 19:27:04 -0300

    refactored the mock setup

--- tests/test_api_webdriver.py
+++ tests/test_api_webdriver.py
@@ -6,11 +6,11 @@ def test_visit():
     """
     WebDriver.visit should call browser.get
     """
-    with Stub() as firefox:
-        from selenium.firefox.webdriver import WebDriver as firefox
-        with Mock() as mock:
-            mock.get('http://foo.com') >> 'bar'
-        firefox() >> mock
+    with Mock() as mock:
+        mock.get('http://foo.com')
+    with Stub() as browser:
+        from selenium.firefox.webdriver import WebDriver as browser
+        browser() >> mock
     driver = WebDriver()
     driver.visit('http://foo.com')
     mock.validate()
