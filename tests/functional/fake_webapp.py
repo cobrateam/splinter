d85ae712cba502a75a0d9292c3e7edb290e833c5
Gustavo Rezende <nsigustavo@gmail.com>
2010-09-19 12:37:42 -0300

    refactored functional test html

--- tests/functional/fake_webapp.py
+++ tests/functional/fake_webapp.py
@@ -5,14 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return """\
-<html>
-  <body>
-    <head>
-      <title>Example Title</title>
-    </head>
-  </body
-</html>"""
+    return """
+        <html>
+            <head>
+              <title>Example Title</title>
+            </head>
+        </html>"""
 
 if __name__ == '__main__':
     app.run()
