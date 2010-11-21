d96331b15648f4d75e850fa34592af336523a95a
Hugo Lopes Tavares <hltbra@gmail.com>
2010-11-21 16:27:45 -0200

    changed version to 0.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='splinter',
-      version='0.1',
+      version='0.0.1',
       description='browser abstraction for web acceptance testing',
       long_description='',
       author='CobraTeam',
