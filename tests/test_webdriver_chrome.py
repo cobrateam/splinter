9bddfb25b5cbaf6cae668e1a6b19ab1e54a242f9
Francisco Souza <francisco@franciscosouza.net>
2011-01-09 03:01:02 -0200

    Added a base class for WebDriver tests

--- tests/test_webdriver_chrome.py
+++ tests/test_webdriver_chrome.py
@@ -1,5 +1,5 @@
 import unittest
-from should_dsl import should, should_not
+from should_dsl import should
 from splinter.browser import Browser
 from fake_webapp import EXAMPLE_APP
 from base import BaseBrowserTests
@@ -16,23 +16,6 @@ class ChromeBrowserTest(BaseBrowserTests, unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
-        
-    def test_can_execute_javascript(self):
-        "should execute javascript"
-        self.browser.execute_script("$('body').empty()")
-        self.browser.find_by_tag("body") == ""
-        
-    def test_can_evaluate_script(self):
-        "should evaluate script"
-        assert self.browser.evaluate_script("4+4") == 8
-        
-    def test_can_verify_if_a_element_is_visible(self):
-        "should provide verify if element is visible"
-        self.browser.find_by_id("visible") |should| be_visible
-    
-    def test_can_verify_if_a_element_is_invisible(self):
-        "should provide verify if element is invisible"
-        self.browser.find_by_id("invisible") |should_not| be_visible
 
     def test_attach_file_is_not_implemented(self):
         "attach file is no implemented for chrome driver"
--- tests/test_webdriver_firefox.py
+++ tests/test_webdriver_firefox.py
@@ -1,12 +1,12 @@
 import unittest
-from should_dsl import should, should_not
+from should_dsl import should
 from splinter.browser import Browser
 from fake_webapp import EXAMPLE_APP
-from base import BaseBrowserTests
+from webdriver import WebDriverTests
 
 import os
 
-class FirefoxBrowserTest(BaseBrowserTests, unittest.TestCase):
+class FirefoxBrowserTest(WebDriverTests, unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -18,30 +18,13 @@ class FirefoxBrowserTest(BaseBrowserTests, unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
-        
-    def test_can_execute_javascript(self):
-        "should execute javascript"
-        self.browser.execute_script("$('body').empty()")
-        self.browser.find_by_tag("body") == ""
-        
-    def test_can_evaluate_script(self):
-        "should evaluate script"
-        assert self.browser.evaluate_script("4+4") == 8
-        
-    def test_can_verify_if_a_element_is_visible(self):
-        "should provide verify if element is visible"
-        self.browser.find_by_id("visible") |should| be_visible
-    
-    def test_can_verify_if_a_element_is_invisible(self):
-        "should provide verify if element is invisible"
-        self.browser.find_by_id("invisible") |should_not| be_visible
-    
+
     def test_attach_file(self):
         "should provide a way to change file field value"
         file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'mockfile.txt')
         self.browser.attach_file('file', file_path)
         self.browser.find_by_name('upload').click()
-    
+
         html = self.browser.html
         html |should| include('text/plain')
         html |should| include(open(file_path).read())
