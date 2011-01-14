e5e6c05a5c2c9499b3cac8fb3505e50e5ddc2ad2
Francisco Souza <francisco@franciscosouza.net>
2011-01-14 13:32:32 -0200

    Added lxml as installation dependency (closes #30)

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='splinter',
       author_email='andrewsmedina@gmail.com',
       packages=find_packages(),
       test_suite='nose.collector',
-      install_requires=['selenium', 'zope.testbrowser'],
+      install_requires=['selenium', 'zope.testbrowser', 'lxml'],
       tests_require=['nose', 'coverage', 'ludibrio', 'flask'],
       )
