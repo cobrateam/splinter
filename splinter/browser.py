e9b27aec4220e0c9c3daa1526c9ce8b512f73dfe
Bernardo Heynemann <heynemann@gmail.com>
2011-02-11 12:03:01 -0200

    Fixed browser requirement

--- splinter/browser.py
+++ splinter/browser.py
@@ -1,11 +1,19 @@
 from splinter.driver.webdriver.firefox import WebDriver as FirefoxWebDriver
 from splinter.driver.webdriver.chrome import WebDriver as ChromeWebDriver
-from splinter.driver.zopetestbrowser import ZopeTestBrowser
 
-_DRIVERS = {'webdriver.firefox': FirefoxWebDriver,
-            'zope.testbrowser': ZopeTestBrowser,
-            'webdriver.chrome': ChromeWebDriver}
+try:
+    from splinter.driver.zopetestbrowser import ZopeTestBrowser
+    has_zope_browser = True
+except ImportError:
+    has_zope_browser = False
 
+_DRIVERS = {
+    'webdriver.firefox': FirefoxWebDriver,
+    'webdriver.chrome': ChromeWebDriver
+}
+
+if has_zope_browser:
+    _DRIVERS['zope.testbrowser'] = ZopeTestBrowser
 
 def Browser(driver_name='webdriver.firefox'):
     driver = _DRIVERS[driver_name]
