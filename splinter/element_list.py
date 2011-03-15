4da6c37c2a38fec684847fcc1a92e3cda3fe1b16
andrews medina <andrewsmedina@gmail.com>
2011-03-14 23:10:37 -0300

    refactoring exceptions element list

--- splinter/element_list.py
+++ splinter/element_list.py
@@ -2,7 +2,7 @@ class ElementDoesNotExist(Exception):
     pass
 
 class ElementList(list):
-    
+
     def __init__(self, list):
         self.extend(list)
 
@@ -11,17 +11,11 @@ class ElementList(list):
             return super(ElementList, self).__getitem__(index)
         except IndexError:
             raise ElementDoesNotExist('element does not exist')
-    
+
     @property
     def first(self):
-        try:
-            return self[0]
-        except IndexError:
-            raise ElementDoesNotExist('element does not exist')
-    
+        return self[0]
+
     @property
     def last(self):
-        try:
-            return self[-1]
-        except IndexError:
-            raise ElementDoesNotExist('element does not exist')
+        return self[-1]
