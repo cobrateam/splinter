fcd2aa3b716ccc46e2c363b314b14b63eda6cfd6
Douglas Camata <d.camata@gmail.com>
2011-04-09 19:47:11 -0300

    new test for iframes using with statement

--- tests/iframes.py
+++ tests/iframes.py
@@ -2,7 +2,7 @@ from nose.tools import assert_equals
 
 class IFrameElementsTest(object):
 
-    def test_can_change_frames(self):
+    def test_can_change_frames_using(self):
         self.browser.switch_to_frame('iframemodal')
         value = self.browser.find_by_tag('h1').first.value
         assert_equals(value, 'IFrame Example Header')
@@ -12,3 +12,9 @@ class IFrameElementsTest(object):
         self.browser.switch_to_frame(None)
         value = self.browser.find_by_tag('h1').first.value
         assert_equals(value, 'Example Header')
+
+    def test_can_change_frames_using_new_with_approach(self):
+        """can change frames using new with's approach"""
+        with self.browser.get_iframe('iframemodal') as frame:
+            value = frame.find_by_tag('h1').first.value
+            assert_equals(value, 'IFrame Example Header')
