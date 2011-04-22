8cce35cb0e82200c427990ca89118438961a94c3
Flavia Missi <flaviamissi@gmail.com>
2011-04-22 17:27:05 -0300

    Added some methods, and ensures that the response is different then 404 raising an exception in this case

--- splinter/request_handler/request_handler.py
+++ splinter/request_handler/request_handler.py
@@ -1,6 +1,9 @@
 import httplib
 from urlparse import urlparse
 
+class PageNotFound(Exception):
+    pass
+
 class RequestHandler(object):
     def __init__(self, url):
         self.url = url
@@ -11,13 +14,21 @@ class RequestHandler(object):
         return self.response.status
 
     def connect(self):
+        self._create_connection()
+        self._store_response()
+        self._ensures_success_response()
+
+    def _ensures_success_response(self):
+        if(self.status_code == 404):
+            raise PageNotFound("page not found")
+
+    def _store_response(self):
+        self.response = self.conn.getresponse()
+
+    def _create_connection(self):
         self._parse_url()
         self.conn = httplib.HTTPConnection(self.host)
         self.conn.request('GET', self.url)
-        self.store_response()
-
-    def store_response(self):
-        self.response = self.conn.getresponse()
 
     def _parse_url(self):
         parsed_url = urlparse(self.url)
