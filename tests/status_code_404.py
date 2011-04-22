7d55a07378c305717bf4ed9279ca8a99b96fa1ea
Flavia Missi <flaviamissi@gmail.com>
2011-04-22 17:27:54 -0300

    Added a raises expectation and removed the assert

--- tests/status_code_404.py
+++ tests/status_code_404.py
@@ -1,7 +1,8 @@
-from nose.tools import assert_true
+from nose.tools import raises
+from splinter.request_handler.request_handler import PageNotFound
 
 class StatusCode404Test(object):
+    @raises(PageNotFound)
     def test_should_visit_an_absent_page_and_get_an_404_error(self):
         "Should visit an absent page and get an 404 error"
         self.browser.visit("http://localhost:5000/this_page_does_not_exists")
-        assert_true(False)
