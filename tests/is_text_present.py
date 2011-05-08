fa0e037d9d55260a7afec7891c3c9ed4fbfc2f01
andrews medina <andrewsmedina@gmail.com>
2011-05-08 19:50:28 -0300

    removing spaces

--- tests/is_text_present.py
+++ tests/is_text_present.py
@@ -14,7 +14,7 @@ class IsTextPresentTest(object):
     def test_is_text_present_and_should_wait_time(self):
         "should verify if text is present and wait for five seconds"
         self.browser.find_link_by_text('FOO').first.click()
-        assert_true(self.browser.is_text_present('BAR!', wait_time = 5))
+        assert_true(self.browser.is_text_present('BAR!', wait_time=5))
 
     def test_is_text_not_present(self):
         "should verify if text is not present"
@@ -27,5 +27,4 @@ class IsTextPresentTest(object):
     def test_is_text_not_present_and_should_wait_time(self):
         "should verify if text is not present and wait for five seconds"
         self.browser.find_link_by_text('FOO').first.click()
-        assert_true(self.browser.is_text_not_present('outro texto', wait_time = 5))
-
+        assert_true(self.browser.is_text_not_present('outro texto', wait_time=5))
