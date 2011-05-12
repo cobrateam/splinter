7b80da2a0312ba07bb2fb9302c87d00f78608826
Francisco Souza <francisco@franciscosouza.net>
2011-05-12 12:01:50 -0300

    Small docs

--- samples/test_facebook_events.py
+++ samples/test_facebook_events.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+This snippet show how to "test" a Facebook feature: the creation of an event.
+
+It creates an event by going to http://www.facebook.com, login and navigate to "Create an event" page.
+"""
+
 import os
 import unittest
 import time
