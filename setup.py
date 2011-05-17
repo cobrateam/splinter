a589eb6104df3c9d54c501ad1154edc7d4e50412
andrews medina <andrewsmedina@gmail.com>
2011-05-17 00:11:40 -0300

    updated version to 0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 README = open('README.rst').read()
 
 setup(name='splinter',
-      version='0.0.3dev',
+      version='0.0.3',
       description='browser abstraction for web acceptance testing',
       long_description=README,
       author='CobraTeam',
