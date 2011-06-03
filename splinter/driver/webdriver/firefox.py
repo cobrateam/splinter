f5961531c780f31246646224fcb207fa4a722871
Francisco Souza <francisco@franciscosouza.net>
2011-06-03 15:57:48 -0300

    Changed the patch of popen

--- splinter/driver/webdriver/firefox.py
+++ splinter/driver/webdriver/firefox.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import logging
 import subprocess
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from splinter.driver.webdriver import BaseWebDriver, WebDriverElement
-from tempfile import TemporaryFile
 
 class WebDriver(BaseWebDriver):
     old_popen = subprocess.Popen
@@ -22,38 +20,3 @@ class WebDriver(BaseWebDriver):
         self.element_class = WebDriverElement
 
         super(WebDriver, self).__init__()
-
-    def _patch_subprocess(self):
-        loggers_to_silence = [
-            'selenium.webdriver.firefox.utils',
-            'selenium.webdriver.firefox.firefoxlauncher',
-            'selenium.webdriver.firefox.firefox_profile',
-            'selenium.webdriver.remote.utils',
-            'selenium.webdriver.remote.remote_connection',
-            'addons.xpi',
-            'webdriver.ExtensionConnection',
-        ]
-
-        class MutedHandler(logging.Handler):
-            def emit(self, record):
-                pass
-
-        for name in loggers_to_silence:
-            logger = logging.getLogger(name)
-            logger.addHandler(MutedHandler())
-            logger.setLevel(99999)
-
-        # selenium is such a verbose guy let's make it open the
-        # browser without showing all the meaningless output
-        def MyPopen(*args, **kw):
-            kw['stdout'] = TemporaryFile()
-            kw['stderr'] = TemporaryFile()
-            kw['close_fds'] = True
-            return self.old_popen(*args, **kw)
-
-        subprocess.Popen = MyPopen
-
-    def _unpatch_subprocess(self):
-        # cleaning up the house
-        subprocess.Popen = self.old_popen
-
