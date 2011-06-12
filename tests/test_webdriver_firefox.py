734f60961cb1e7b4cdd17bc9b0e1a6f0af487ceb
Francisco Souza <francisco@franciscosouza.net>
2011-06-12 20:56:50 -0300

    Missing test for "dismiss" feature on alert elements

--- tests/test_webdriver_firefox.py
+++ tests/test_webdriver_firefox.py
@@ -37,6 +37,13 @@ class FirefoxBrowserTest(WebDriverTests, unittest.TestCase):
         assert_equals(alert.text, 'This is an alert example.')
         alert.accept()
 
+    def test_acess_alerts_and_dismiss_them(self):
+        self.browser.visit(EXAMPLE_APP + 'alert')
+        self.browser.find_by_tag('h1').first.click()
+        alert = self.browser.get_alert()
+        assert_equals(alert.text, 'This is an alert example.')
+        alert.dismiss()
+
     def test_access_prompts_and_be_able_to_fill_then(self):
         self.browser.visit(EXAMPLE_APP + 'alert')
         self.browser.find_by_tag('h2').first.click()
