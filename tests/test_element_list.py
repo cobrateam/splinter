f7f7c990f8c849fc210c6a1c28661c9533658273
Francisco Souza <francisco@franciscosouza.net>
2011-07-04 02:44:21 -0300

    More tests for ElementList

--- tests/test_element_list.py
+++ tests/test_element_list.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import unittest
-from nose.tools import assert_equals, assert_false, assert_true
+from nose.tools import assert_equals, assert_false, assert_true, raises
 from splinter.element_list import ElementList
+from splinter.exceptions import ElementDoesNotExist
 
 class Person(object):
     """Very simple class, just for tests"""
@@ -20,9 +21,20 @@ class ElementListTest(unittest.TestCase):
         assert_false(the_list.is_empty())
         assert_true(ElementList([]).is_empty())
 
+    def test_property_first_and_last(self):
+        "should provide a \"first\" and a \"last\" properties which returns the first and last element"
+        the_list = ElementList([1, 2, 3])
+        assert_equals(the_list[0], the_list.first)
+        assert_equals(the_list[2], the_list.last)
+
     def test_call_method_on_first_element(self):
         "when some method is missing on ElementList and is present in element, it should be passed"
         the_list = ElementList([Person(), Person(), Person()])
         the_list.walk()
         the_person = the_list.first
         assert_equals("walking", the_person.current_action)
+
+    @raises(ElementDoesNotExist)
+    def test_raise_exception_on_indexerror(self):
+        "should raise ElementDoesNotExist exception on IndexError"
+        ElementList([]).first
