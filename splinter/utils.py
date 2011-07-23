13b8d3923d73c7db571a34a22ed95af95537375d
andrews medina <andrewsmedina@gmail.com>
2011-07-23 14:36:08 -0300

    fixed pep8 issues in utils.py

--- splinter/utils.py
+++ splinter/utils.py
@@ -1,5 +1,6 @@
 import warnings
 
+
 def warn_deprecated(method, deprecated_method_name):
     def deprecated_method(*args, **kwargs):
         warnings.warn("'%s' is deprecated, use '%s' instead." % (deprecated_method_name, method.__name__), DeprecationWarning, stacklevel=2)
