98ee3e509953208f8ebcfaa0d0fde32276d22b4c
andrews medina <andrewsmedina@gmail.com>
2011-07-23 14:42:25 -0300

    fixed pep8 issues in test/element_does_not_exist.py

--- tests/element_does_not_exist.py
+++ tests/element_does_not_exist.py
@@ -1,6 +1,7 @@
 from splinter.exceptions import ElementDoesNotExist
 from nose.tools import raises
 
+
 class ElementDoestNotExistTest(object):
 
     @raises(ElementDoesNotExist)
@@ -14,5 +15,3 @@ class ElementDoestNotExistTest(object):
     @raises(ElementDoesNotExist)
     def test_element_list_raises_when_element_does_not_exists(self):
         self.browser.find_by_css('.element-that-dont-exists')[2]
-
-
