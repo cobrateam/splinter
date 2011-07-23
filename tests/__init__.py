ac66ec0532ffcb0020d726e733709d3fdef93558
andrews medina <andrewsmedina@gmail.com>
2011-07-23 14:40:16 -0300

    fixed pep8 issues in test/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,8 +3,10 @@ from fake_webapp import start_server, stop_server
 
 TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
 
+
 def setup():
     start_server()
+
 
 def teardown():
     stop_server()
