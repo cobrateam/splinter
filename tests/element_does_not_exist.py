a4173f30afc1e6663fd61c28b80bd557b0d55237
Francisco Souza <francisco@franciscosouza.net>
2011-08-13 15:51:14 -0300

    Another test to guarantee ElementDoesNotExist and ElementList behavior

--- tests/element_does_not_exist.py
+++ tests/element_does_not_exist.py
@@ -1,4 +1,6 @@
-from nose.tools import raises
+# -*- coding: utf-8 -*-
+
+from nose.tools import assert_equals, raises
 from splinter.exceptions import ElementDoesNotExist
 
 
@@ -15,3 +17,15 @@ class ElementDoestNotExistTest(object):
     @raises(ElementDoesNotExist)
     def test_element_list_raises_when_element_does_not_exists(self):
         self.browser.find_by_css('.element-that-dont-exists')[2]
+
+    def test_element_list_contains_right_information_and_raises_right_exception(self):
+        "element list contains right information about query and raises nice exception message"
+        expected_message = 'no elements could be find with css ".element-that-dont-exists"'
+        element_list = self.browser.find_by_css('.element-that-dont-exists')
+        assert_equals('css', element_list.find_by)
+        assert_equals('.element-that-dont-exists', element_list.query)
+
+        try:
+            element_list.first
+        except ElementDoesNotExist, e:
+            assert_equals(expected_message, e.args[0])
