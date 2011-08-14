bae79f145b2de3aa13b0314c2c53dbb55fc136f9
Francisco Souza <francisco@franciscosouza.net>
2011-08-14 00:30:22 -0300

    Another test to ensure behavior

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -22,8 +22,17 @@ class SubClass(SuperClass):
         print "hello"
 
 
+class SubSubClass(SubClass):
+
+    def say_hello(self):
+        print "I can't say hello"
+
 class MetaTest(unittest.TestCase):
 
     def test_should_include_docs_from_superclass(self):
         "should include doc from superclass"
         assert_equals(SuperClass.say_hello.__doc__, SubClass.say_hello.__doc__)
+
+    def test_should_include_docs_from_any_class_in_hierarchy(self):
+        "should include doc from any class in hierarchy"
+        assert_equals(SuperClass.say_hello.__doc__, SubSubClass.say_hello.__doc__)
