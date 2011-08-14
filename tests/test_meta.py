8ce5ceb40e207437db8d0cbb7ad0326faa85b01f
Francisco Souza <francisco@franciscosouza.net>
2011-08-14 00:28:11 -0300

    Refactoring test code

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -5,22 +5,25 @@ import unittest
 from nose.tools import assert_equals
 from splinter.meta import InheritedDocs
 
+
+class SuperClass(object):
+    __metaclass__ = InheritedDocs
+
+    def say_hello(self):
+        """
+        Says hello
+        """
+        pass
+
+
+class SubClass(SuperClass):
+
+    def say_hello(self):
+        print "hello"
+
+
 class MetaTest(unittest.TestCase):
 
     def test_should_include_docs_from_superclass(self):
         "should include doc from superclass"
-        class SuperClass(object):
-            __metaclass__ = InheritedDocs
-
-            def say_hello(self):
-                """
-                Says hello
-                """
-                pass
-
-        class SubClass(SuperClass):
-
-            def say_hello(self):
-                print "hello"
-
         assert_equals(SuperClass.say_hello.__doc__, SubClass.say_hello.__doc__)
