a3f5e95cf2a58fcf08d0307ba71719834a8190e5
Francisco Souza <francisco@franciscosouza.net>
2011-08-13 23:06:08 -0300

    docstrings for ElementList

--- splinter/element_list.py
+++ splinter/element_list.py
@@ -2,8 +2,22 @@ from splinter.exceptions import ElementDoesNotExist
 
 
 class ElementList(list):
+    """
+    List of elements. Each member of the list is (usually) an instance of :class:`ElementAPI <splinter.driver.ElementAPI>`.
+
+    Beyond the traditional list methods, the ``ElementList`` provides some other methods, listed below.
+
+    There is a peculiar behavior on ElementList: you never get an ``IndexError``. Instead, you can an
+    ElementDoesNotExist exception when trying to access an inexistent item in the list:
+
+        >>> element_list = ElementList([])
+        >>> element_list[0] # raises ElementDoesNotExist
+    """
 
     def __init__(self, list, context=None, driver=None, find_by=None, query=None):
+        """
+        Creates the list.
+        """
         self.extend(list)
         self.context = context
         self.driver = driver
@@ -18,16 +32,35 @@ class ElementList(list):
 
     @property
     def first(self):
+        """
+        An alias to the first element of the list:
+
+            >>> assert element_list[0] == element_list.first
+        """
         return self[0]
 
     @property
     def last(self):
+        """
+        An alias to the last element of the list:
+
+            >>> assert element_list[-1] == element_list.last
+        """
         return self[-1]
 
     def find_by_css(self, element):
+        """
+        Performs a find on the page using the provided selector.
+
+        For more details, check the docs for :meth:`DriverAPI.find_by_css <splinter.driver.DriverAPI.find_by_css>`
+        method.
+        """
         return self.driver.find_by_css(element)
 
     def is_empty(self):
+        """
+        Returns ``True`` if the list is empty.
+        """
         return len(self) == 0
 
     def __getattr__(self, name):
