0db57ffe78fa58838d55744f27822dbca3872777
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-09-26 18:24:12 -0300

    Updating required versions (selenium, coverage, sphinx, lxml)

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='splinter',
       author_email='andrewsmedina@gmail.com',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['selenium==2.6.0', 'lxml==2.3'],
+      install_requires=['selenium==2.7.0', 'lxml==2.3.1'],
       tests_require=['coverage', 'flask'],
       )
