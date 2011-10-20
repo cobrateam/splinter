ca1a8c1c9a33f52f0349688bb62f250bb13092dc
Jayson Reis <jayson.reis@sabbre.com.br>
2011-10-20 16:00:43 -0200

    some work on generic driver

--- splinter/driver/generic/driver.py
+++ splinter/driver/generic/driver.py
@@ -2,25 +2,25 @@ import lxml.html
 from lxml.cssselect import CSSSelector
 from splinter.driver import DriverAPI
 from splinter.element_list import ElementList
-from splinter.driver.generic.element import GenericElement, GenericLinkElement
+from splinter.driver.generic.element import GenericElement, GenericLinkElement, GenericOptionElement
 
 
 class GenericDriver(DriverAPI):
     def find_option_by_value(self, value):
         html = lxml.html.fromstring(self.html)
         element = html.xpath('//option[@value="%s"]' % value)[0]
-        control = self._browser.getControl(element.text)
-        return ElementList([GenericOptionElement(control, self)])
+        return ElementList([GenericOptionElement(element, self)])
 
     def find_option_by_text(self, text):
         html = lxml.html.fromstring(self.html)
         element = html.xpath('//option[normalize-space(text())="%s"]' % text)[0]
-        control = self._browser.getControl(element.text)
-        return ElementList([GenericOptionElement(control, self)])
+        return ElementList([GenericOptionElement(element, self)])
 
     def find_by_css(self, selector):
         xpath = CSSSelector(selector).path
         return self.find_by_xpath(xpath, original_find="css", original_selector=selector)
+
+    find_by_css_selector = find_by_css
 
     def find_by_xpath(self, xpath, original_find=None, original_selector=None):
         html = lxml.html.fromstring(self.html)
@@ -50,17 +50,12 @@ class GenericDriver(DriverAPI):
         return self.find_by_xpath('//*[@id="%s"][1]' % id_value, original_find="id", original_selector=id_value)
 
     def find_by_name(self, name):
-        elements = []
-        index = 0
+        html = lxml.html.fromstring(self.html)
 
-        while True:
-            try:
-                control = self._browser.getControl(name=name, index=index)
-                elements.append(control)
-                index += 1
-            except IndexError:
-                break
-        return ElementList([GenericControlElement(element, self) for element in elements])
+        elements = []
+        for element in html.xpath('//*[@name="%s"]' % name):
+            elements.append(element)
+        return ElementList([GenericElement(element, self) for element in elements])
 
     def find_link_by_text(self, text):
         return self._find_links_by_xpath("//a[text()='%s']" % text)
@@ -74,33 +69,10 @@ class GenericDriver(DriverAPI):
     def find_link_by_partial_text(self, partial_text):
         return self._find_links_by_xpath("//a[contains(text(), '%s')]" % partial_text)
 
-    def fill(self, name, value):
-        self.find_by_name(name=name).first._control.value = value
-
-    def choose(self, name, value):
-        control = self._browser.getControl(name=name)
-        control.value = [option for option in control.options if option == value]
-
-    def check(self, name):
-        control = self._browser.getControl(name=name)
-        control.value = control.options
-
-    def uncheck(self, name):
-        control = self._browser.getControl(name=name)
-        control.value = []
-
-    def attach_file(self, name, file_path):
-        control = self._browser.getControl(name=name)
-        content_type, _ = mimetypes.guess_type(file_path)
-        control.add_file(open(file_path), content_type, None)
-
     def _find_links_by_xpath(self, xpath):
         html = lxml.html.fromstring(self.html)
         links = html.xpath(xpath)
         return ElementList([GenericLinkElement(link, self) for link in links])
-
-    def select(self, name, value):
-        self.find_by_name(name).first._control.value = [value]
 
     def _element_is_link(self, element):
         return element.tag == 'a'
