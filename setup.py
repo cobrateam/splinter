da2fb52436a7a95639f3a37d96984c88d2ece108
Rafael Caricio <rafael.jacinto@gmail.com>
2011-11-25 17:42:04 -0200

    Bugfix in project packaging.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from splinter import __version__
 
 README = open('README.rst').read()
@@ -9,7 +9,7 @@ setup(name='splinter',
       long_description=README,
       author='CobraTeam',
       author_email='andrewsmedina@gmail.com',
-      packages=['splinter'],
+      packages=find_packages(exclude=['docs', 'tests', 'samples']),
       include_package_data=True,
       install_requires=['selenium==2.13.1', 'lxml==2.3.1'],
       tests_require=['coverage', 'flask'],
