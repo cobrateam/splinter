f8ea5b7017265f789ebb0ee48af6ecb492a3dbcc
Andrews Medina <andrewsmedina@gmail.com>
2012-01-24 12:01:04 -0200

    using chrome options instead desired_capatibilities to set user_agent

--- splinter/driver/webdriver/chrome.py
+++ splinter/driver/webdriver/chrome.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from selenium.webdriver import Chrome, DesiredCapabilities
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 from splinter.driver.webdriver import BaseWebDriver, WebDriverElement
 from splinter.driver.webdriver.cookie_manager import ChromeCookieManager
+
 
 class WebDriver(BaseWebDriver):
     def __init__(self, user_agent=None):
         self._patch_subprocess()
-        capabilities = DesiredCapabilities.CHROME
+        options = Options()
 
         if user_agent is not None:
-            capabilities["chrome.switches"] = ["--user-agent=" + user_agent]
+            options.add_argument("--user-agent=" + user_agent)
 
-        self.driver = Chrome(desired_capabilities=capabilities)
+        self.driver = Chrome(chrome_options=options)
         self._unpatch_subprocess()
 
         self.element_class = WebDriverElement
