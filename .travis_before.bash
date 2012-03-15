6de734baef15f4ae3c6f3599fc99572e9e188501
Douglas Camata <d.camata@gmail.com>
2012-03-15 19:02:20 -0300

    trying to fix google chrome tests on travis-ci

---
+++ .travis_before.bash
@@ -0,0 +1,11 @@
+ #!/bin/bash
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb -o google-chrome.deb
+sudo dpkg -i google-chrome.deb
+
+sudo apt-get unzip
+wget http://chromium.googlecode.com/files/chromedriver_linux32_18.0.1022.0.zip -O chromedriver.zip
+unzip chromedriver.zip
+./chromedriver &
