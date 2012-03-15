c39cd52ad649e820152b4a68542e9bc07bc66e3b
Douglas Camata <d.camata@gmail.com>
2012-03-15 19:35:49 -0300

    oh my god, give me patience

--- .travis_before.bash
+++ .travis_before.bash
@@ -2,10 +2,10 @@
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
+sudo apt-get install libxss1 xdg-utils
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb -O google-chrome.deb
 sudo dpkg -i google-chrome.deb
 
-sudo apt-get install libxss1 xdg-utils
 wget http://chromium.googlecode.com/files/chromedriver_linux32_18.0.1022.0.zip -O chromedriver.zip
 unzip chromedriver.zip
 chmod +x chromedriver
