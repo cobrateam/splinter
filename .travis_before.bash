c92b200ac2d2f304aa1edbf87a2e5d8f8b51e89a
Douglas Camata <d.camata@gmail.com>
2012-03-15 19:32:33 -0300

    adding google chrome dependencies

--- .travis_before.bash
+++ .travis_before.bash
@@ -5,7 +5,7 @@ sh -e /etc/init.d/xvfb start
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb -O google-chrome.deb
 sudo dpkg -i google-chrome.deb
 
-sudo apt-get install unzip
+sudo apt-get install libxss1 xdg-utils
 wget http://chromium.googlecode.com/files/chromedriver_linux32_18.0.1022.0.zip -O chromedriver.zip
 unzip chromedriver.zip
 chmod +x chromedriver
