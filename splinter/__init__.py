bbf77c3ebfad192789bac08c47ad00b63498f25e
Francisco Souza <f@souza.cc>
2012-08-03 13:41:23 -0300

    bump to 0.4.7

--- splinter/__init__.py
+++ splinter/__init__.py
@@ -2,5 +2,5 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-__version__ = '0.4.6.1'
+__version__ = '0.4.7'
 from splinter.browser import Browser
