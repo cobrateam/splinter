a5e48ce35334503be7a5dcc565689c9fab42dee5
Andrews Medina <andrewsmedina@gmail.com>
2012-11-16 04:45:02 -0200

    imported abort method

--- tests/fake_webapp.py
+++ tests/fake_webapp.py
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-from flask import Flask, request
+from flask import Flask, request, abort
 from os import path
 
 
