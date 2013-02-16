a4f285c859f7a8c13b37ee51c0453005c6503cae
Andrews Medina <andrewsmedina@gmail.com>
2013-02-16 15:36:28 -0200

    setup: bump to 0.5.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ README = codecs.open('README.rst', encoding='utf-8').read()
 
 setup(
     name='splinter',
-    version='0.4.10',
+    version='0.5.0',
     description='browser abstraction for web acceptance testing',
     long_description=README,
     author='CobraTeam',
