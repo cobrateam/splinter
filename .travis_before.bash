cf73dc57fa83378409d6842e6e8a2d5798a469c8
Andrews Medina <andrewsmedina@gmail.com>
2013-03-02 11:37:04 -0300

    travis: using the firefox default.

--- .travis_before.bash
+++ .travis_before.bash
@@ -7,7 +7,7 @@
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
-sudo apt-get install libxss1 xdg-utils
+sudo apt-get install libxss1 xdg-utils -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb -O google-chrome.deb
 sudo dpkg -i google-chrome.deb
 
@@ -18,9 +18,9 @@ sudo chmod 755 /usr/local/bin/chromedriver
 
 # Remove after 4814 is resolved
 # http://code.google.com/p/selenium/issues/detail?id=4814
-wget http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/19.0/linux-i686/en-US/firefox-19.0.tar.bz2
-tar -xjf firefox-19.0.tar.bz2
-export PATH=./firefox:$PATH
+#wget http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/19.0/linux-i686/en-US/firefox-19.0.tar.bz2
+#tar -xjf firefox-19.0.tar.bz2
+#export PATH=./firefox:$PATH
 
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.31.0.jar -O selenium-server.jar
 java -jar selenium-server.jar > /dev/null &
