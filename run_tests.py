9a6f4ec52816f322ec02e083376016048f77bd75
Andrews Medina <andrewsmedina@gmail.com>
2013-03-01 23:11:21 -0300

    s/2012/2013.

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2012 splinter authors. All rights reserved.
+# Copyright 2013 splinter authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
