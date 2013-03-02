28c43de057fe9490e5f68b00dde270dfbe5378a4
Andrews Medina <andrewsmedina@gmail.com>
2013-03-01 23:39:14 -0300

    travis_before: update drivers.

--- .travis_before.bash
+++ .travis_before.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2012 splinter authors. All rights reserved.
+# Copyright 2013 splinter authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
@@ -11,17 +11,16 @@ sudo apt-get install libxss1 xdg-utils
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb -O google-chrome.deb
 sudo dpkg -i google-chrome.deb
 
-wget https://chromedriver.googlecode.com/files/chromedriver_linux32_21.0.1180.4.zip -O chromedriver.zip
+wget https://chromedriver.googlecode.com/files/chromedriver_linux32_26.0.1383.0.zip -O chromedriver.zip
 unzip chromedriver.zip
 sudo cp chromedriver /usr/local/bin
 sudo chmod 755 /usr/local/bin/chromedriver
 
 # Remove after 4814 is resolved
 # http://code.google.com/p/selenium/issues/detail?id=4814
-wget http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/16.0/linux-i686/en-US/firefox-16.0.tar.bz2
-tar -xjf firefox-16.0.tar.bz2
+wget http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/19.0/linux-i686/en-US/firefox-19.0.tar.bz2
+tar -xjf firefox-19.0.tar.bz2
 export PATH=./firefox:$PATH
 
-wget http://selenium.googlecode.com/files/selenium-server-standalone-2.28.0.jar -O selenium-server.jar
+wget http://selenium.googlecode.com/files/selenium-server-standalone-2.31.0.jar -O selenium-server.jar
 java -jar selenium-server.jar > /dev/null &
-sleep 20
