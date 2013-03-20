af20fdf1b76953d8bfc85dcf80cbdd7e53125018
Jared McGuire <jrdmcgr@gmail.com>
2013-03-20 11:34:22 -0400

    Added support for passing basic HTTP authentication in the url.

--- splinter/request_handler/request_handler.py
+++ splinter/request_handler/request_handler.py
@@ -5,6 +5,7 @@
 # license that can be found in the LICENSE file.
 
 import httplib
+import base64
 from urlparse import urlparse
 from status_code import StatusCode
 
@@ -39,10 +40,17 @@ class RequestHandler(object):
         self.conn = httplib.HTTPConnection(self.host, self.port)
         self.conn.putrequest('GET', self.path)
         self.conn.putheader('User-agent', 'python/splinter')
+        if self.auth:
+            self.conn.putheader("Authorization", "Basic %s" % self.auth)
         self.conn.endheaders()
 
     def _parse_url(self):
         parsed_url = urlparse(self.request_url)
+        if parsed_url.username and parsed_url.password:
+            login = '%s:%s' % (parsed_url.username, parsed_url.password)
+            self.auth = base64.standard_b64encode(login)
+        else:
+            self.auth = None
         self.host = parsed_url.hostname
         self.port = parsed_url.port
         self.path = parsed_url.path
--- tests/fake_webapp.py
+++ tests/fake_webapp.py
@@ -4,8 +4,9 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-from flask import Flask, request, abort
+from flask import Flask, request, abort, Response
 from os import path
+from functools import wraps
 
 
 this_folder = path.abspath(path.dirname(__file__))
@@ -21,6 +22,30 @@ EXAMPLE_ALERT_HTML = read_static('alert.html')
 EXAMPLE_TYPE_HTML = read_static('type.html')
 EXAMPLE_POPUP_HTML = read_static('popup.html')
 EXAMPLE_NO_BODY_HTML = read_static('no-body.html')
+
+# Functions for http basic auth. 
+# Taken verbatim from http://flask.pocoo.org/snippets/8/
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 app = Flask(__name__)
 
@@ -87,6 +112,11 @@ def query_string():
 @app.route('/popup')
 def popup():
     return EXAMPLE_POPUP_HTML
+
+@app.route('/authenticate')
+@requires_auth
+def auth_required():
+    return "Success!"
 
 
 def start_flask_app(host, port):
