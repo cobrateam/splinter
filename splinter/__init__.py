26cadaadeecb71bbfdcae404e650caf87499236d
Andrews Medina <andrewsmedina@gmail.com>
2013-04-15 15:58:21 -0300

    setup: bump to 0.5.1

--- splinter/__init__.py
+++ splinter/__init__.py
@@ -2,5 +2,5 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-__version__ = ''
+__version__ = '0.5.1'
 from splinter.browser import Browser
