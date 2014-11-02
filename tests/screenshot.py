11aaa30a8f71089128f92ed6a59dedbfa06d0026
Andrews Medina <andrewsmedina@gmail.com>
2014-11-02 12:25:33 -0200

    tests: improved screenshot test to get tempdir using tempfile module.

--- tests/screenshot.py
+++ tests/screenshot.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2013 splinter authors. All rights reserved.
+# Copyright 2014 splinter authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
+
+import tempfile
+
 
 class ScreenshotTest(object):
 
     def test_take_screenshot(self):
         "should take a screenshot of the current page"
         filename = self.browser.screenshot()
-        self.assertTrue('tmp' in filename)
+        self.assertTrue(tempfile.gettempdir() in filename)
 
     def test_take_screenshot_with_prefix(self):
         "should add the prefix to the screenshot file name"
