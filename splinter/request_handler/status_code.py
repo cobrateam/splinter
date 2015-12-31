1b4cdef91116359cefe7c940bfe00012404c032c
Andrews Medina <andrewsmedina@gmail.com>
2015-12-31 17:47:56 -0200

    simplify status_code code

--- splinter/request_handler/status_code.py
+++ splinter/request_handler/status_code.py
@@ -7,9 +7,6 @@
 
 class StatusCode(object):
 
-    http_errors = (400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411,
-                   412, 413, 414, 415, 416, 417, 500, 501, 502, 503, 504, 505)
-
     def __init__(self, status_code, reason):
         #: A message for the response (example: Success)
         self.reason = reason
@@ -20,12 +17,10 @@ class StatusCode(object):
         return self.code == other
 
     def __str__(self):
-        return "%s - %s" % (self.code, self.reason)
+        return "{} - {}".format(self.code, self.reason)
 
     def is_success(self):
         """
         Returns ``True`` if the response was succeed, otherwise, returns ``False``.
         """
-        if self.code not in self.http_errors:
-            return True
-        return False
+        return self.code < 400
