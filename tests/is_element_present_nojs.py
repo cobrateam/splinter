f3beeac5ba67cacbbc3fa3bc76efe847016cd976
dryobates <jakub.stolarski@kidosoft.pl>
2016-02-07 13:08:35 +0100

    Ignoring wait_time for non-javascript drivers.

--- tests/is_element_present_nojs.py
+++ tests/is_element_present_nojs.py
@@ -11,10 +11,6 @@ class IsElementPresentNoJSTest(object):
         "should is element present by css verify if element is present"
         self.assertTrue(self.browser.is_element_present_by_css('h1'))
 
-    def test_is_element_present_by_css_using_a_custom_wait_time(self):
-        "should is element present by css raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_present_by_css, '.async-element2', wait_time=3)
-
     def test_is_element_present_by_css_returns_false_if_element_is_not_present(self):
         "should is element present by css returns False if element is not present"
         self.assertFalse(self.browser.is_element_present_by_css('.async-elementzz'))
@@ -27,17 +23,9 @@ class IsElementPresentNoJSTest(object):
         "should is element not present by css returns False if element is present"
         self.assertFalse(self.browser.is_element_not_present_by_css('h1'))
 
-    def test_is_element_not_present_by_css_using_a_custom_wait_time(self):
-        "should is element not present by css raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_not_present_by_css, '.async-element', wait_time=3)
-
     def test_is_element_present_by_xpath(self):
         "should is element present by xpath verify if element is present"
         self.assertTrue(self.browser.is_element_present_by_xpath('//h1'))
-
-    def test_is_element_present_by_xpath_using_a_custom_wait_time(self):
-        "should is element present by xpath raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_present_by_xpath, '//h5', wait_time=3)
 
     def test_is_element_present_by_xpath_returns_false_if_element_is_not_present(self):
         "should is element present by xpath returns false if element is not present"
@@ -47,17 +35,9 @@ class IsElementPresentNoJSTest(object):
         "should is element not present by xpath returns false if element is present"
         self.assertFalse(self.browser.is_element_not_present_by_xpath('//h1'))
 
-    def test_is_element_not_present_by_xpath_using_a_custom_wait_time(self):
-        "should is element not present by xpath raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_not_present_by_xpath, '//h4', wait_time=3)
-
     def test_is_element_present_by_tag(self):
         "should is element present by tag verify if element is present"
         self.assertTrue(self.browser.is_element_present_by_tag('h1'))
-
-    def test_is_element_present_by_tag_using_a_custom_wait_time(self):
-        "should is element present by tag raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_present_by_tag, 'h4', wait_time=3)
 
     def test_is_element_present_by_tag_returns_false_if_element_is_not_present(self):
         "should is element present by tag returns false if element is not present"
@@ -66,10 +46,6 @@ class IsElementPresentNoJSTest(object):
     def test_is_element_not_present_by_tag(self):
         "should is element not present by tag verify if element is not present"
         self.assertTrue(self.browser.is_element_not_present_by_tag('h4'))
-
-    def test_is_element_not_present_by_tag_using_a_custom_wait_time(self):
-        "should is element not present by tag raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_not_present_by_tag, 'h4', wait_time=3)
 
     def test_is_element_not_present_by_tag_returns_false_if_element_is_present(self):
         "should is element not present by tag returns false if element is present"
@@ -95,10 +71,6 @@ class IsElementPresentNoJSTest(object):
         "should is element present by value verify if element is present"
         self.assertTrue(self.browser.is_element_present_by_value('M'))
 
-    def test_is_element_present_by_value_using_a_custom_wait_time(self):
-        "should is element present by value raise NotImplementedError if a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_present_by_value, 'async-header-value', wait_time=3)
-
     def test_is_element_present_by_value_returns_false_if_element_is_not_present(self):
         "should is element present by value returns False if element is not present"
         self.assertFalse(self.browser.is_element_present_by_value('async-header-value'))
@@ -106,10 +78,6 @@ class IsElementPresentNoJSTest(object):
     def test_is_element_not_present_by_value(self):
         "should is element not present by value verify if element is not present"
         self.assertTrue(self.browser.is_element_not_present_by_value('async-header-value'))
-
-    def test_is_element_not_present_by_value_using_a_custom_wait_time(self):
-        "should is element not present by value raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_not_present_by_value, 'async-header-value', wait_time=3)
 
     def test_is_element_not_present_by_value_returns_false_if_element_is_present(self):
         "should is element not present by value returns False if element is present"
@@ -119,10 +87,6 @@ class IsElementPresentNoJSTest(object):
         "should is element present by id verify if element is present"
         self.assertTrue(self.browser.is_element_present_by_id('firstheader'))
 
-    def test_is_element_present_by_id_using_a_custom_wait_time(self):
-        "should is element present by id raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_present_by_id, 'async-header', wait_time=3)
-
     def test_is_element_present_by_id_returns_false_if_element_is_not_present(self):
         "should is element present by id returns False if element is not present"
         self.assertFalse(self.browser.is_element_present_by_id('async-header'))
@@ -130,10 +94,6 @@ class IsElementPresentNoJSTest(object):
     def test_is_element_not_present_by_id(self):
         "should is element not present by id verify if element is not present"
         self.assertTrue(self.browser.is_element_not_present_by_id('async-header'))
-
-    def test_is_element_not_present_by_id_using_a_custom_wait_time(self):
-        "should is element not present by id raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_not_present_by_id, 'async-header', wait_time=3)
 
     def test_is_element_not_present_by_id_returns_false_if_element_is_present(self):
         "should is element not present by id returns False if element is present"
@@ -143,10 +103,6 @@ class IsElementPresentNoJSTest(object):
         "should is element present by name verify if element is present"
         self.assertTrue(self.browser.is_element_present_by_name('query'))
 
-    def test_is_element_present_by_name_using_a_custom_wait_time(self):
-        "should is element present by name raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_present_by_name, 'async-input', wait_time=3)
-
     def test_is_element_present_by_name_returns_false_if_element_is_not_present(self):
         "should is element present by name returns false if element is not present"
         self.assertFalse(self.browser.is_element_present_by_name('async-input'))
@@ -154,10 +110,6 @@ class IsElementPresentNoJSTest(object):
     def test_is_element_not_present_by_name(self):
         "should is element not present by name verify if element is not present"
         self.assertTrue(self.browser.is_element_not_present_by_name('async-input'))
-
-    def test_is_element_not_present_by_name_using_a_custom_wait_time(self):
-        "should is element not present by name raise NotImplementedError if using a custom wait time"
-        self.assertRaises(NotImplementedError, self.browser.is_element_not_present_by_name, 'async-input', wait_time=3)
 
     def test_is_element_not_present_by_name_returns_false_if_element_is_present(self):
         "should is element not present by name returns false if element is present"
