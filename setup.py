352c19e05930cff6b1ad7f23fb54e2031dbfdbfd
Andrews Medina <andrewsmedina@gmail.com>
2016-05-01 13:42:10 -0300

    update selenium

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ] + [('Programming Language :: Python :: %s' % x) for x in '2.6 2.7 3.3 3.4'.split()],
     packages=find_packages(exclude=['docs', 'tests', 'samples']),
     include_package_data=True,
-    install_requires=['selenium>=2.53.1'],
+    install_requires=['selenium>=2.53.2'],
     extras_require={'zope.testbrowser': ['zope.testbrowser>=4.0.4',
                                          'lxml>=2.3.6', 'cssselect'],
                     'django': ['Django>=1.7.11,<1.10', 'lxml>=2.3.6', 'cssselect', 'six'],
