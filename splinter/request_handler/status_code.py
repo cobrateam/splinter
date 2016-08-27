cd1d00295686730d3d1132e2866f0338da10f5cb
bmcculley <bmcculley@outlook.com>
2016-08-26 22:14:49 -0400

    update for issue #529

--- splinter/request_handler/status_code.py
+++ splinter/request_handler/status_code.py
@@ -3,15 +3,31 @@
 # Copyright 2012 splinter authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
+import httplib
+from urlparse import urlparse
 
 
 class StatusCode(object):
 
-    def __init__(self, status_code, reason):
-        #: A message for the response (example: Success)
-        self.reason = reason
-        #: Code of the response (example: 200)
-        self.code = status_code
+    def __init__(self, url):
+        #: so we can grab the header return from the url
+        self.url = url
+
+        # need to parse the host and path from the url
+        parsed_url = urlparse(self.url)
+        con_url = parsed_url.netloc
+        # we may also need to think about including the query string
+        path = parsed_url.path
+
+        try:
+            conn = httplib.HTTPConnection(con_url)
+            conn.request("HEAD", path)
+            resp = conn.getresponse()
+            self.code = resp.status
+            self.reason = resp.reason
+        except StandardError:
+            self.code = None
+            self.reason = None
 
     def __eq__(self, other):
         return self.code == other
