a93e2fb4c50a929da1025f396c097013e6f5a856
Andrews Medina <andrewsmedina@gmail.com>
2018-08-25 23:31:10 -0300

    update flask

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     extras_require={
         "zope.testbrowser": ["zope.testbrowser>=4.0.4", "lxml>=2.3.6", "cssselect"],
         "django": ["Django>=1.7.11,<1.12", "lxml>=2.3.6", "cssselect", "six"],
-        "flask": ["Flask>=0.12.2", "lxml>=2.3.6", "cssselect"],
+        "flask": ["Flask>=1.0.2", "lxml>=2.3.6", "cssselect"],
     },
     tests_require=["coverage", "flask"],
 )
