7df3e551ff5862bdc9da581326393c0873a6cac9
Joshua Fehler <jsfehler@gmail.com>
2019-11-19 13:48:46 -0500

    Enable keep_alive for remote webdriver

--- splinter/driver/webdriver/remote.py
+++ splinter/driver/webdriver/remote.py
@@ -27,7 +27,7 @@ class WebDriver(BaseWebDriver):
         abilities = getattr(DesiredCapabilities, browsername, {})
         abilities.update(ability_args)
 
-        self.driver = Remote(url, abilities)
+        self.driver = Remote(url, abilities, keep_alive=True)
 
         self.element_class = WebDriverElement
 
