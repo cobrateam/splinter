a5748621573c8dfd9a5bce069b25e7e466bcaa14
Joshua Fehler <jsfehler@users.noreply.github.com>
2019-12-02 12:16:53 -0500

    Allow ActionChains when using Remote WebDriver (#738)

--- splinter/driver/webdriver/remote.py
+++ splinter/driver/webdriver/remote.py
@@ -8,7 +8,7 @@ from selenium.webdriver import Remote
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from splinter.driver.webdriver import (
     BaseWebDriver,
-    WebDriverElement as BaseWebDriverElement,
+    WebDriverElement,
 )
 from splinter.driver.webdriver.cookie_manager import CookieManager
 
@@ -46,38 +46,3 @@ class WebDriver(BaseWebDriver):
         self._cookie_manager = CookieManager(self.driver)
 
         super(WebDriver, self).__init__(wait_time)
-
-
-class WebDriverElement(BaseWebDriverElement):
-    def mouse_over(self):
-        """
-        Remote Firefox doesn't support mouseover.
-        """
-        raise NotImplementedError("Remote Firefox doesn't support mouse over")
-
-    def mouse_out(self):
-        """
-        Remote Firefox doesn't support mouseout.
-        """
-        raise NotImplementedError("Remote Firefox doesn't support mouseout")
-
-    def double_click(self):
-        """
-        Remote Firefox doesn't support doubleclick.
-        """
-        raise NotImplementedError("Remote Firefox doesn't support doubleclick")
-
-    def right_click(self):
-        """
-        Remote Firefox doesn't support right click'
-        """
-        raise NotImplementedError("Remote Firefox doesn't support right click")
-
-    def drag_and_drop(self, droppable):
-        """
-        Remote Firefox doesn't support drag and drop
-        """
-        raise NotImplementedError("Remote Firefox doesn't support drag an drop")
-
-    mouseover = mouse_over
-    mouseout = mouse_out
