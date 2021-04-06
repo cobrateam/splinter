8df59979af7d2a61372c7d33ce5ab9f9d27a9eea
Joshua Fehler <jsfehler@gmail.com>
2021-04-06 14:18:06 -0400

    Test Remote Chrome along with Remote Firefox

--- tests/test_webdriver_remote.py
+++ tests/test_webdriver_remote.py
@@ -27,10 +27,29 @@ def selenium_server_is_running():
     return "WebDriver Hub" in page_contents
 
 
-class RemoteBrowserTest(WebDriverTests, unittest.TestCase):
+class RemoteBrowserFirefoxTest(WebDriverTests, unittest.TestCase):
     @pytest.fixture(autouse=True, scope='class')
     def setup_browser(self, request):
-        request.cls.browser = Browser("remote")
+        request.cls.browser = Browser('remote', browser='firefox')
+        request.addfinalizer(request.cls.browser.quit)
+
+    def setUp(self):
+        self.browser.visit(EXAMPLE_APP)
+
+    def test_support_with_statement(self):
+        "Remote should support with statement"
+        with Browser("remote"):
+            pass
+
+    def test_should_be_able_to_change_user_agent(self):
+        "Remote should not support custom user agent"
+        pass
+
+
+class RemoteBrowserChromeTest(WebDriverTests, unittest.TestCase):
+    @pytest.fixture(autouse=True, scope='class')
+    def setup_browser(self, request):
+        request.cls.browser = Browser('remote', browser='chrome')
         request.addfinalizer(request.cls.browser.quit)
 
     def setUp(self):
