d82d05b27effcca07b1d945e92b83e9d6b1e244f
Joshua Fehler <jsfehler@gmail.com>
2021-12-01 11:27:31 -0500

    Migrate test_element_list.py to use pytest instead of unittest

--- tests/test_element_list.py
+++ tests/test_element_list.py
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-import unittest
+import pytest
 
 from splinter.element_list import ElementList
 from splinter.exceptions import ElementDoesNotExist
@@ -20,85 +20,93 @@ class Person(object):
         self.current_action = "walking"
 
 
-class ElementListTest(unittest.TestCase):
-    def test_slice(self):
-        """ElementList should work with slice operations."""
-        elem_list = ElementList([1, 2, 3, 4, 5])
-        new_elem_list = elem_list[2:5]
-        self.assertTrue(len(new_elem_list) == 3)
+def test_slice():
+    """ElementList should work with slice operations."""
+    elem_list = ElementList([1, 2, 3, 4, 5])
+    new_elem_list = elem_list[2:5]
+    assert len(new_elem_list) == 3
 
-    def test_method_that_verifies_if_the_list_is_empty(self):
-        "should verify if the list is empty"
-        the_list = ElementList([1, 2, 3])
-        self.assertFalse(the_list.is_empty())
-        self.assertTrue(ElementList([]).is_empty())
 
-    def test_property_first_and_last(self):
-        """
-        should provide a \"first\" and a \"last\" properties
-        which returns the first and last element
-        """
-        the_list = ElementList([1, 2, 3])
-        self.assertEqual(the_list[0], the_list.first)
-        self.assertEqual(the_list[2], the_list.last)
+def test_method_that_verifies_if_the_list_is_empty():
+    "should verify if the list is empty"
+    the_list = ElementList([1, 2, 3])
+    assert not the_list.is_empty()
+    assert ElementList([]).is_empty()
 
-    def test_call_method_on_first_element(self):
-        """
-        when some method is missing on ElementList and
-        is present in element, it should be passed
-        """
-        the_list = ElementList([Person(), Person(), Person()])
-        the_list.walk()
-        the_person = the_list.first
-        self.assertEqual("walking", the_person.current_action)
 
-    def test_raise_exception_on_indexerror(self):
-        "should raise ElementDoesNotExist exception on IndexError"
-        with self.assertRaises(ElementDoesNotExist):
-            ElementList([]).first
+def test_property_first_and_last():
+    """
+    should provide a \"first\" and a \"last\" properties
+    which returns the first and last element
+    """
+    the_list = ElementList([1, 2, 3])
+    assert the_list[0] == the_list.first
+    assert the_list[2] == the_list.last
 
-    def test_raise_exception_on_indexerror_with_unicode_query(self):
-        "should raise ElementDoesNotExist exception on IndexError"
-        with self.assertRaises(ElementDoesNotExist):
-            ElementList([], query=u".element[title=título]").first
 
-    def test_raise_attribute_error(self):
-        """
-        should raise AttributeError when trying to access
-        a non-existent method on list and element
-        """
-        with self.assertRaises(AttributeError):
-            the_list = ElementList([Person(), Person()])
-            the_list.talk()
+def test_call_method_on_first_element():
+    """
+    when some method is missing on ElementList and
+    is present in element, it should be passed
+    """
+    the_list = ElementList([Person(), Person(), Person()])
+    the_list.walk()
+    the_person = the_list.first
+    assert "walking" == the_person.current_action
 
-    def test_attribute_error_method_for_empty(self):
-        """
-        should raise ElementDoesNotExist when the list is empty
-        and someone tries to access a method or property on the child element.
-        """
-        with self.assertRaises(ElementDoesNotExist):
-            the_list = ElementList([])
-            the_list.unknown_method()
 
-    def test_attribute_error_content(self):
-        "should raise AttributeError with right content"
-        with self.assertRaises(AttributeError) as cm:
-            the_list = ElementList([Person(), Person()])
-            the_list.talk()
+def test_raise_exception_on_indexerror():
+    "should raise ElementDoesNotExist exception on IndexError"
+    with pytest.raises(ElementDoesNotExist):
+        ElementList([]).first
 
-        expected_message = "'ElementList' object has no attribute 'talk'"
-        e = cm.exception
-        self.assertEqual(expected_message, e.args[0])
 
-    def test_not_found_exception_with_query_and_method(self):
-        """
-        should receive the find method
-        and the query and use them in exception
-        """
-        with self.assertRaises(ElementDoesNotExist) as cm:
-            the_list = ElementList([], find_by="id", query="menu")
-            the_list.first
+def test_raise_exception_on_indexerror_with_unicode_query():
+    "should raise ElementDoesNotExist exception on IndexError"
+    with pytest.raises(ElementDoesNotExist):
+        ElementList([], query=u".element[title=título]").first
 
-        expected_message = 'no elements could be found with id "menu"'
-        e = cm.exception
-        self.assertEqual(expected_message, e.args[0])
+
+def test_raise_attribute_error():
+    """
+    should raise AttributeError when trying to access
+    a non-existent method on list and element
+    """
+    with pytest.raises(AttributeError):
+        the_list = ElementList([Person(), Person()])
+        the_list.talk()
+
+
+def test_attribute_error_method_for_empty():
+    """
+    should raise ElementDoesNotExist when the list is empty
+    and someone tries to access a method or property on the child element.
+    """
+    with pytest.raises(ElementDoesNotExist):
+        the_list = ElementList([])
+        the_list.unknown_method()
+
+
+def test_attribute_error_content():
+    "should raise AttributeError with right content"
+    with pytest.raises(AttributeError) as cm:
+        the_list = ElementList([Person(), Person()])
+        the_list.talk()
+
+    expected_message = "'ElementList' object has no attribute 'talk'"
+    e = cm.exception
+    assert expected_message == e.args[0]
+
+
+def test_not_found_exception_with_query_and_method():
+    """
+    should receive the find method
+    and the query and use them in exception
+    """
+    with pytest.raises(ElementDoesNotExist) as cm:
+        the_list = ElementList([], find_by="id", query="menu")
+        the_list.first
+
+    expected_message = 'no elements could be found with id "menu"'
+    e = cm.exception
+    assert expected_message == e.args[0]
