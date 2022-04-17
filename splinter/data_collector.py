8b35d3475b848a15a2f9409fac021e1cf821d12e
Pisciotta <32421258+Pisciotta@users.noreply.github.com>
2022-04-17 13:09:44 +0200

    flake8 syntax
    Made compliant to flake8 syntax

--- splinter/data_collector.py
+++ splinter/data_collector.py
@@ -8,32 +8,31 @@ import time
 import random
 import re
 
+
 class DataCollector:
-    def __init__(self, startUrl, browser):
-        self.startUrl = startUrl
+    def __init__(self, start_url, browser):
         self.browser = browser
-        self.browser.visit(self.startUrl)
-        self.dataCollection = dict()
-        self.urlTemplates = []
-        
-    def waitUntilURL(self, url):
+        self.browser.visit(start_url)
+        self.data_collection = {}
+        self.url_templates = []
+
+    def wait_until_url(self, url):
         while self.browser.url != url:
             time.sleep(5)
-    
-    def addToDataCollection(self, dataLabel, values):
-        if dataLabel not in self.dataCollection:
-            self.dataCollection[dataLabel] = []
-        self.dataCollection[dataLabel].append(values)
-    
-    def removeHTML(self, html):
+
+    def add_to_data_collection(self, data_label, values):
+        if data_label not in self.data_collection:
+            self.data_collection[data_label] = []
+        self.data_collection[data_label].append(values)
+
+    def remove_html(self, html):
         return re.sub(re.compile("<.*?>"),'', html)
-        
-    def _getInnermostTagTextContainingStrings(self, tag, strings=[]):
+
+    def _get_innermost_tag_text_containing_strings(self, tag, strings=[]):
         '''
         This method searches for the given tag in a non greedy way, containing
         all the strings in the same order they occur in the input list.
         
-
         Parameters
         ----------
         tag : STRING
@@ -49,35 +48,24 @@ class DataCollector:
             Text inside the matched string after deleting all contained HTML tags.         
 
         '''
-
         match = None    
         while match is None:
             match = re.search("<"+tag+"(?:(?!<"+tag+").)*"+".*?".join(strings)+".*?</"+tag+">", self.browser.html)
-        return self.removeHTML(match.group(0))
-        
-        
-    
-    def iterateOverPages(self, varName, varValues, callbacks):
-        for urlTemplate in self.urlTemplates:
-            placeholder = "{"+varName+"}"
-            if placeholder in urlTemplate:
-                for varValue in varValues:
-                    self.browser.visit(urlTemplate.replace(placeholder, varValue))
+        return self.remove_html(match.group(0))
+
+    def iterate_over_pages(self, var_name, var_values, callbacks):
+        for url_template in self.url_templates:
+            placeholder = "{"+var_name+"}"
+            if placeholder in url_template:
+                for var_value in var_values:
+                    self.browser.visit(url_template.replace(placeholder, var_value))
                     for callback, params in callbacks:
                         data = getattr(self, callback)(**params)
                         if data is not None:
-                            self.addToDataCollection(placeholder+"="+varValue, data)
-                
-                    
-    def waitRandomTime(self, minTime, maxTime):
-        time.sleep(random.randint(minTime,maxTime))
-        
-        
-    def addUrlTemplate(self, url):
-        self.urlTemplates.append(url)
-        
-        
-        
-        
-            
+                            self.add_to_data_collection(placeholder+"="+var_value, data)
 
+    def wait_random_time(self, min_time, max_time):
+        time.sleep(random.randint(min_time,max_time))
+
+    def add_url_template(self, url):
+        self.url_templates.append(url)
