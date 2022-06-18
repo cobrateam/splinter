867d3c53190a1977c37c95397ce8fcb4f33c792c
jordanvance <jordanvance@users.noreply.github.com>
2022-06-17 21:05:53 -0400

    Use named logger instead of root logger (#1054)
    * Use named logger instead of root logger
    
    * Change LOG to logger
    
    Co-authored-by: Jordan Vance <jvance@shoobx.com>

--- splinter/browser.py
+++ splinter/browser.py
@@ -31,6 +31,8 @@ _DRIVERS = {
     'zope.testbrowser': None,
 }
 
+logger = logging.getLogger(__name__)
+
 try:
     from splinter.driver.webdriver.chrome import WebDriver as ChromeWebDriver
     from splinter.driver.webdriver.firefox import WebDriver as FirefoxWebDriver
@@ -40,7 +42,7 @@ try:
     _DRIVERS['firefox'] = FirefoxWebDriver
     _DRIVERS['remote'] = RemoteWebDriver
 except ImportError as e:
-    logging.debug(f'Import Warning: {e}')
+    logger.debug(f'Import Warning: {e}')
 
 
 try:
@@ -48,7 +50,7 @@ try:
 
     _DRIVERS["edge"] = EdgeWebDriver
 except ImportError as e:
-    logging.debug(f'Import Warning: {e}')
+    logger.debug(f'Import Warning: {e}')
 
 
 try:
@@ -56,7 +58,7 @@ try:
 
     _DRIVERS["zope.testbrowser"] = ZopeTestBrowser
 except ImportError as e:
-    logging.debug(f'Import Warning: {e}')
+    logger.debug(f'Import Warning: {e}')
 
 try:
     import django  # noqa
@@ -64,7 +66,7 @@ try:
 
     _DRIVERS["django"] = DjangoClient
 except ImportError as e:
-    logging.debug(f'Import Warning: {e}')
+    logger.debug(f'Import Warning: {e}')
 
 try:
     import flask  # noqa
@@ -72,7 +74,7 @@ try:
 
     _DRIVERS["flask"] = FlaskClient
 except ImportError as e:
-    logging.debug(f'Import Warning: {e}')
+    logger.debug(f'Import Warning: {e}')
 
 
 def get_driver(driver, retry_count=3, *args, **kwargs):
