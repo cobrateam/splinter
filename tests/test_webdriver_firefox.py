aad383e610745d1db680b65d1a112e419d264dd5
Joshua Fehler <jsfehler@gmail.com>
2022-06-20 19:08:46 -0400

    test

--- tests/test_webdriver_firefox.py
+++ tests/test_webdriver_firefox.py
@@ -74,7 +74,10 @@ def test_preference_set(request):
         assert int(value) == 360
     except:  # NOQA
         browser.visit("about:config")
-        raise Exception(browser.html)
+        browser.find_by_id("warningButton").click()
+        browser.find_by_id("about-config-search").fill("dom.max_script_run_time")
+        elem = browser.find_by_xpath("//table[@id='prefs']/tr[1]")
+        raise Exception(elem.html)
 
 
 def test_capabilities_set(request):
