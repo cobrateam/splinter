f1b85ce4ab9548787fe929888649204b8f6f1755
Joshua Fehler <jsfehler@gmail.com>
2022-06-23 19:27:02 -0400

    fixup

--- splinter/meta.py
+++ splinter/meta.py
@@ -6,7 +6,7 @@
 
 
 class InheritedDocs(type):
-    def __new__(mcs, class_name: str, bases: tuple, dict: dict) -> type:  # NOQA: N804, A002
+    def __new__(mcs, class_name: str, bases: tuple, dict: dict):  # NOQA: N804, A002
         """Metaclass that forces inheritance of docstrings."""
         items_to_patch = [
             (k, v) for k, v in dict.items() if not k.startswith("__") and not v.__doc__
