ddcc252fc7c25710f9e493e4ecd9c648b10b55a7
Andmedoctopus <andmed999@gmail.com>
2023-12-21 16:00:42 +0200

    Skip one more test

--- tests/test_webdriver_remote.py
+++ tests/test_webdriver_remote.py
@@ -159,6 +159,11 @@ class RemoteBrowserSafariTest(WebDriverTests, unittest.TestCase):
 
     # ------- BEGIN OF CLICK PROBLEM TESTS -------
     #https://stackoverflow.com/questions/77388720/automation-testing-with-selenium-click-doesnt-works-on-new-safari-17-ios-sonoma
+
+    @pytest.mark.xfail
+    def test_click_input_by_css_selector(self):
+        super().test_click_input_by_css_selector()
+
     @pytest.mark.xfail
     def test_clicking_submit_button_doesnt_post_button_value_if_empty(self):
         super().test_clicking_submit_button_doesnt_post_button_value_if_empty()
