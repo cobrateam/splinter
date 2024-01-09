ac7263baa783ef4db21ff9fe720635c3116f5a5b
Andrei Medvedev <43909845+Andmedoctopus@users.noreply.github.com>
2024-01-09 18:13:44 +0200

    Add safari driver (#1220)
    * Add safari driver
    
    * Add test for Safari remote
    
    * Add MacOS CI
    
    * Add more params for safari
    
    * Change order of hub and node
    
    * Add wait for selenium hub up for macos
    
    * Add logs for hub and node
    
    * Fix selenium filename
    
    * Add timeout util for mac
    
    * Install java for macos
    
    * Debug java version
    
    * Check java version other way
    
    * Other way of java installation
    
    * Try to reinstall java
    
    * Try to install java11
    
    * Try to run with java 11
    
    * Rewrite all tests requested for new browser for safari
    
    * Patch multisession tests instead of rewriting
    
    * Add return to main page for test
    
    * Check async element present on the page
    
    * Move with statement test to setup for Safari
    
    * Add particular browser selector for remote browsers
    
    * Mark click test with xfail for Safari
    
    * Skip one more test
    
    * Add return to main page
    
    * xfail one more click
    
    * Add missed import
    
    * Fix formatting
    
    * Add macos filter for tox
    
    * Add pytest markers
    
    * Set parallel to 1 for macos to avoid new tab problem
    
    * Try to limit parallel jobs
    
    * Make explicit visit of main page for tab test
    
    * Add test click to xfail
    
    * Try to rerun failed tests
    
    * Try to rerun less failed tests
    
    * Clear cache before test run
    
    * Run google search only if called directly
    
    * Remove visit of main page in start of tests
    
    * Add reason of skipping for user agent
    
    * Remove all matrix limits

--- tests/test_webdriver_remote.py
+++ tests/test_webdriver_remote.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 import unittest
+from unittest.mock import patch
 from urllib.request import urlopen
 
 import pytest
@@ -32,11 +33,11 @@ class RemoteBrowserFirefoxTest(WebDriverTests, unittest.TestCase):
 
     def test_support_with_statement(self):
         "Remote should support with statement"
-        with Browser("remote"):
+        with Browser("remote", browser="firefox"):
             pass
 
+    @pytest.mark.skip(reason="Remote should not support custom user agent")
     def test_should_be_able_to_change_user_agent(self):
-        "Remote should not support custom user agent"
         pass
 
 
@@ -51,9 +52,169 @@ class RemoteBrowserChromeTest(WebDriverTests, unittest.TestCase):
 
     def test_support_with_statement(self):
         "Remote should support with statement"
-        with Browser("remote"):
+        with Browser("remote", browser="chrome"):
             pass
 
+    @pytest.mark.skip(reason="Remote should not support custom user agent")
     def test_should_be_able_to_change_user_agent(self):
-        "Remote should not support custom user agent"
         pass
+
+
+@pytest.mark.macos
+class RemoteBrowserSafariTest(WebDriverTests, unittest.TestCase):
+    @pytest.fixture(autouse=True, scope="class")
+    def setup_browser(self, request):
+        # test with statement. It can't be used as simple test
+        # because safari doesn't support multisessions
+        with Browser("remote", browser="safari"):
+            pass
+
+        request.cls.browser = Browser("remote", browser="safari")
+        request.addfinalizer(request.cls.browser.quit)
+
+    def setUp(self):
+        self.browser.visit(EXAMPLE_APP)
+
+    def test_support_with_statement(self):
+        """
+        Remote should support with statement
+        See setup browser
+        """
+
+    @pytest.mark.skip(reason="Remote should not support custom user agent")
+    def test_should_be_able_to_change_user_agent(self):
+        pass
+
+    # ------- BEGIN OF MULTISESSION TESTS -------
+    # Safari doesn't support multisessions.
+    # So next tests mock quit of browser.
+    def get_new_browser(self):
+        return self.browser
+
+    def test_can_forward_on_history(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_can_forward_on_history()
+
+    def test_create_and_access_a_cookie(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_create_and_access_a_cookie()
+
+    def test_create_many_cookies_at_once_as_dict(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_create_many_cookies_at_once_as_dict()
+
+    def test_create_some_cookies_and_delete_them_all(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_create_some_cookies_and_delete_them_all()
+
+    def test_create_and_delete_a_cookie(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_create_and_delete_a_cookie()
+
+    def test_create_and_delete_many_cookies(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_create_and_delete_many_cookies()
+
+    def test_try_to_destroy_an_absent_cookie_and_nothing_happens(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_try_to_destroy_an_absent_cookie_and_nothing_happens()
+
+    def test_create_and_get_all_cookies(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_create_and_get_all_cookies()
+
+    def test_create_and_use_contains(self):
+        with patch("splinter.driver.webdriver.remote.WebDriver.quit"):
+            super().test_create_and_use_contains()
+
+    # ------- END OF MULTISESSION TESTS -------
+
+    def test_can_fill_more_than_one_field_in_form(self):
+        "should provide a away to change field value"
+        self.browser.fill("query", "my name")
+        self.assertFalse(self.browser.find_by_id("gender-m").checked)
+        self.assertFalse(self.browser.find_option_by_value("rj").selected)
+        self.assertFalse(self.browser.find_by_name("some-check").checked)
+        self.assertTrue(self.browser.find_by_name("checked-checkbox").checked)
+        # Select of dropdown doesn't work for Safari 17 (remote). Safari as OS user works well
+        # for some reason select doesn't work for Safari
+        self.browser.fill_form(
+            {
+                "query": "another new query",
+                "description": "Just another description value in the textarea",
+                "gender": "M",
+                # "uf": "rj",
+                "some-check": True,
+                "checked-checkbox": False,
+            },
+        )
+        query_value = self.browser.find_by_name("query").value
+        self.assertEqual("another new query", query_value)
+        desc_value = self.browser.find_by_name("description").value
+        self.assertEqual("Just another description value in the textarea", desc_value)
+        self.assertTrue(self.browser.find_by_id("gender-m").checked)
+        # self.assertTrue(self.browser.find_option_by_value("rj").selected)
+        self.assertTrue(self.browser.find_by_name("some-check").checked)
+        self.assertFalse(self.browser.find_by_name("checked-checkbox").checked)
+
+    # ------- BEGIN OF CLICK PROBLEM TESTS -------
+    # https://stackoverflow.com/questions/77388720/automation-testing-with-selenium-click-doesnt-works-on-new-safari-17-ios-sonoma
+    @pytest.mark.xfail
+    def test_click_element_by_css_selector(self):
+        super().test_click_element_by_css_selector()
+
+    @pytest.mark.xfail
+    def test_click_input_by_css_selector(self):
+        super().test_click_input_by_css_selector()
+
+    @pytest.mark.xfail
+    def test_clicking_submit_button_doesnt_post_button_value_if_empty(self):
+        super().test_clicking_submit_button_doesnt_post_button_value_if_empty()
+
+    @pytest.mark.xfail
+    def test_clicking_submit_button_doesnt_post_button_value_if_name_not_present(self):
+        super().test_clicking_submit_button_doesnt_post_button_value_if_name_not_present()
+
+    @pytest.mark.xfail
+    def test_clicking_submit_button_posts_button_value_if_value_present(self):
+        super().test_clicking_submit_button_posts_button_value_if_value_present()
+
+    @pytest.mark.xfail
+    def test_clicking_submit_button_posts_empty_value_if_value_not_present(self):
+        super().test_clicking_submit_button_posts_empty_value_if_value_not_present()
+
+    @pytest.mark.xfail
+    def test_clicking_submit_input_doesnt_post_input_value_if_empty(self):
+        super().test_clicking_submit_input_doesnt_post_input_value_if_empty()
+
+    @pytest.mark.xfail
+    def test_clicking_submit_input_doesnt_post_input_value_if_name_not_present(self):
+        super().test_clicking_submit_input_doesnt_post_input_value_if_name_not_present()
+
+    @pytest.mark.xfail
+    def test_clicking_submit_input_posts_empty_value_if_value_not_present(self):
+        super().test_clicking_submit_input_posts_empty_value_if_value_not_present()
+
+    @pytest.mark.xfail
+    def test_clicking_submit_input_posts_input_value_if_value_present(self):
+        super().test_clicking_submit_input_posts_input_value_if_value_present()
+
+    @pytest.mark.xfail
+    def test_submiting_a_form_and_verifying_page_content(self):
+        super().test_submiting_a_form_and_verifying_page_content()
+
+    @pytest.mark.xfail
+    def test_click_links(self):
+        super().test_click_links()
+
+    # ------- END OF CLICK PROBLEM TESTS -------
+    # ------- START OF TYPE PROBLEM TESTS -------
+    @pytest.mark.xfail
+    def test_simple_type(self):
+        super().test_simple_type()
+
+    @pytest.mark.xfail
+    def test_simple_type_on_element(self):
+        super().test_simple_type_on_element()
+
+    # ------- END OF TYPE PROBLEM TESTS -------
