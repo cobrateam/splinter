3e717741486892fdeff4e192bef1d09b604e0a4c
Joshua Fehler <jsfehler@gmail.com>
2024-02-27 09:04:23 -0500

    fixup

--- splinter/abc/cookie_manager.py
+++ splinter/abc/cookie_manager.py
@@ -1,7 +1,8 @@
 # Copyright 2012 splinter authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
-from abc import ABC, abstractmethod
+from abc import ABC
+from abc import abstractmethod
 
 
 class CookieManagerAPI(ABC):
