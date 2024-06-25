b83d35331d8fd9e00ed7a99775ce30dbb181d7da
Joshua Fehler <jsfehler@gmail.com>
2024-06-25 10:23:27 -0400

    fix tests

--- tests/tests_webdriver/test_keyboard.py
+++ tests/tests_webdriver/test_keyboard.py
@@ -2,51 +2,56 @@ from splinter.driver.webdriver import Keyboard
 
 
 def test_keyboard_down_modifier(browser, app_url):
+    """Scenario: Keys can be held down"""
     browser.visit(app_url)
 
     keyboard = Keyboard(browser.driver)
 
-    keyboard.down("CTRL")
+    keyboard.down("CONTROL")
 
     elem = browser.find_by_css("#keypress_detect")
     assert elem.first
 
 
 def test_keyboard_up_modifier(browser, app_url):
+    """Scenario: Keys can be held up"""
     browser.visit(app_url)
 
     keyboard = Keyboard(browser.driver)
 
-    keyboard.down("CTRL")
-    keyboard.up("CTRL")
+    keyboard.down("CONTROL")
+    keyboard.up("CONTROL")
 
     elem = browser.find_by_css("#keyup_detect")
     assert elem.first
 
 
 def test_keyboard_press_modifier(browser, app_url):
+    """Keys can be pressed"""
     browser.visit(app_url)
 
     keyboard = Keyboard(browser.driver)
 
-    keyboard.press("CTRL")
+    keyboard.press("CONTROL")
 
     elem = browser.find_by_css("#keyup_detect")
     assert elem.first
 
 
 def test_element_press_combo(browser, app_url):
+    """Scenario: Key presses can be used in a combo"""
     browser.visit(app_url)
 
     keyboard = Keyboard(browser.driver)
 
-    keyboard.press("CTRL+a")
+    keyboard.press("CONTROL+a")
 
     elem = browser.find_by_css("#keypress_detect_a")
     assert elem.first
 
 
-def test_keyboard_copy_paste(browser, app_url):
+def test_keyboard_ctrl(browser, app_url):
+    """Scenario: The CTRL value is correct across platforms"""
     browser.visit(app_url)
 
     elem_with_value_to_copy = browser.find_by_css("input[name='q']").first
